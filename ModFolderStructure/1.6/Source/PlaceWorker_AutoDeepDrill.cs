e16213fb4c15d6b81d1f57dcddc41945c552dbe9
verakum <eliojose_1985@hotmail.com>
2025-07-17 21:32:39 -0400

    Updated V1.6

---
+++ ModFolderStructure/1.6/Source/PlaceWorker_AutoDeepDrill.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AutoDeepDrill
+{
+	public class PlaceWorker_AutoDeepDrill : PlaceWorker_ShowDeepResources
+	{
+		public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+		{
+			ThingDef thingDef = map.deepResourceGrid.ThingDefAt(loc);
+
+			if (thingDef == null)
+				return AcceptanceReport.WasAccepted;
+
+			return thingDef.thingCategories.Contains(ThingCategoryDefOf.ResourcesRaw);	
+		}
+	}
+}
